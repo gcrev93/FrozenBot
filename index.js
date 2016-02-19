0146faef81b665d8da2eb231d4d9e143554f586c
gcrev93 <gcrev93@hotmail.com>
2016-02-18 16:05:01 -0800

    using wit.ai

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var record = require('./node_modules/node-record-lpcm16/index.js'),
+/*var record = require('./node_modules/node-record-lpcm16/index.js'),
     fs     = require('fs');
     
 var file = fs.createWriteStream('test.wav', { encoding: 'binary' });
@@ -16,6 +16,24 @@ setTimeout(function () {
   speech.speechFun('test.wav');
 }, 10000); 
 
+*/
 
+var rec       = require('node-record-lpcm16'),
+    request   = require('request');
+ 
+var witToken = '5TDUO6B57WKZX7AODI6SSW3Q4KKYZKTE'; // get one from wit.ai! 
+ 
+exports.parseResult = function (err, resp, body) {
+  console.log(body);
+};
+ 
+rec.start().pipe(request.post({
+  'url'     : 'https://api.wit.ai/speech?client=chromium&lang=en-us&output=json',
+  'headers' : {
+    'Accept'        : 'application/vnd.wit.20160202+json',
+    'Authorization' : 'Bearer ' + witToken,
+    'Content-Type'  : 'audio/wav'
+  }
+}, exports.parseResult));
   
 
