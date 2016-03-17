e41304d56e3582de5a995a94323deef47b4902bf
Gabrielle Crevecoeur <Gabrielle@Gabrielles-MacBook-Air.local>
2016-03-17 07:25:12 -0700

    adding serial port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ setTimeout(function () {
   speech.speechFun('test.wav');
 }, 10000); 
 
-*/
+/*
 
 var rec       = require('node-record-lpcm16'),
     request   = require('request');
@@ -36,4 +36,49 @@ rec.start().pipe(request.post({
   }
 }, exports.parseResult));
   
+
+*/
+var serialPort = require("serialport"),
+    SerialPort = require("serialport").SerialPort;
+
+var x = Math.floor((Math.random() * 3) + 1);
+
+var sp = new SerialPort("/dev/tty.usbmodem1411", {
+      baudrate: 115200,
+      dataBits: 8,
+      parity: 'none',
+      stopBits: 1,
+      flowControl: false,
+      parser: serialPort.parsers.readline("\r\n")
+});
+
+function prompt(message, callback) {
+    var stdin = process.stdin,
+        stdout = process.stdout;
+
+    stdin.resume();
+    stdout.write(message);
+
+    stdin.on('data', function (data) {
+        callback(data.toString().trim());
+    });
+}
+
+console.log("Starting up serial host...\n");
+
+
+sp.on('data', function(input) {  
+    console.log(input);
+    sp.write(x);
+    sp.close();
+    
+    
+},
+prompt('', function (input) {
+    sp.write(input);
+    
+})
+
+);
+
 
