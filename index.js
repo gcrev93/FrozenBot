c2da2236d7130561e114f62487f7cd61ec4b82f0
Gabrielle Crevecoeur <Gabrielle@Gabrielles-MacBook-Air.local>
2016-03-29 19:48:28 -0700

    final changes

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
+/* ----------------------  Needed modules ----------------------- */
 var record = require('./node_modules/node-record-lpcm16/index.js'),
-    fs     = require('fs'),
-    async  = require('async'),
-    speech = require("./speech.js"),
-    port   = require('./sp.js');
-    
-var file = fs.createWriteStream('test.wav', { encoding: 'binary' }); 
-var word;
+    fs = require('fs'),
+    speech = require("./speech.js");
+     
 
-record.start();    
-    
+/* ----------------------  Needed variables ----------------------- */
+var clientId = 't2s',                             // Can be anything
+    clientSecret = '<insert clientSecret from Project Oxford>', // API key from Azure marketplace
+    file = fs.createWriteStream('test.wav', { encoding: 'binary' });
+    number = Math.floor((Math.random() * 5) + 1).toString();
+
+/* ----------------------  Where The Magic Happens ----------------------- */
+
+record.start();
 setTimeout(function () {
-  console.log("recording stopped")
-  record.stop().pipe(file);
-  speech.speechFun('test.wav');
+    console.log("recording stopped")
+    record.stop().pipe(file);
+    SpeechFun('test.wav');
 }, 4000); 
 
+
+/* ----------------------  Needed Functions ----------------------- */
+
+function SpeechFun (fName) {
+    speech.getAccessToken(clientId, clientSecret, function (err, accessToken) {
+        if (err) return console.log(err);
+
+        speech.speechToText(fName, accessToken, function (err, res) {
+            if (err) return console.log(err);
+
+            Analysis(res.results[0].lexical);
+        });
+    });
+}
+
+
+function Analysis (x) {
+    port   = require('./sp.js');
+    var n = x.search('elsa'),
+        m = x.search('frozen'),
+        o = x.search('sing');
+
+    if (n != -1 || m != -1 || o != -1)
+    { 
+        port.Start(number);   
+    }
+        
+    else
+        console.log("no keywords found");
+
+}
 
 
 
--- sp.js
+++ sp.js
@@ -1,10 +1,50 @@
-//exports.Start = function Start(){
+
 var serialPort = require("serialport"),
     SerialPort = require("serialport").SerialPort;
 
-var x = Math.floor((Math.random() * 3) + 1);
 
-var sp = new SerialPort("/dev/tty.usbmodem1411", {
+
+var sp = new SerialPort("<insert port>", {
+    baudrate: 115200,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false,
+    parser: serialPort.parsers.readline("\r\n")
+});
+
+function prompt(message, callback) {
+    var stdin = process.stdin,
+        stdout = process.stdout;
+
+    stdin.resume();
+    stdout.write(message);
+
+    stdin.on('data', function(data) {
+        callback(data.toString().trim());
+    });
+}
+
+exports.Start = function (number) {
+    console.log("Starting up serial host...\n");
+     sp.on('data', function(input) {
+            console.log("in the on function");
+            console.log(input);
+            sp.write(number, function(){
+                sp.close()});
+        },
+        prompt('', function (input) {
+           //sp.close();
+}));
+    
+  
+}
+/*
+var serialPort = require("serialport"),
+    SerialPort = require("serialport").SerialPort;
+
+
+var sp = new SerialPort("/dev/tty.usbmodem1421", {
       baudrate: 115200,
       dataBits: 8,
       parity: 'none',
@@ -25,18 +65,13 @@ function prompt(message, callback) {
     });
 }
 
-
 console.log("Starting up serial host...\n");
 
 
-sp.on('data', function(input) {  
+sp.on('data', function(input) {
     console.log(input);
-    sp.write(3);   
 },
 prompt('', function (input) {
     sp.write(input);
-    
 })
-
-);
-//}
+);*/
